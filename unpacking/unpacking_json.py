8c998af47228a27cf1dee33fe3ff60dec95897c4
Hamanist <hamanist@gmail.com>
2023-10-19 16:33:50 +0300

    add docstring

--- unpacking/unpacking_json.py
+++ unpacking/unpacking_json.py
@@ -4,6 +4,11 @@ from class_question.question_class import Question
 
 
 def unpacking_file(file_json):
+    """
+
+    :param file_json: принимает json файл для его чтения
+    :return: возвращает список экземпляра класса полученный из json файла
+    """
     with open(file_json, "r", encoding="utf-8") as f:
         file = json.load(f)
     questions = []
@@ -18,6 +23,11 @@ def unpacking_file(file_json):
 
 
 def statistic(questions):
+    """
+
+    :param questions: принимает данные json файла
+    :return: выводит статистику с правильными ответами и набранными баллами
+    """
     balls = 0
     answers_on_questions = 0
     total_questions = 0
