56183dcaacd3bc190a86db83d7edb4f55be6944d
Hamanist <hamanist@gmail.com>
2023-10-19 16:17:24 +0300

    added the main code of the program

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from unpacking.unpacking_json import unpacking_file
+from unpacking.unpacking_json import unpacking_file, statistic
+import random
 
 
 def main():
     file_json = "question.json"
     questions = unpacking_file(file_json)
 
+    random.shuffle(questions)
+
     for question in questions:
         print(question.build_question())
         user_answer = input('Ваш ответ: ')
         question.user_response = user_answer
         print(question.build_feedback())
+
+    statistic(questions)
+
 
 if __name__ == '__main__':
     main()
